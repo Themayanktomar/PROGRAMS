1f292193464e6149a73e407ccb98dcf0c794b623
Themayanktomar <90128562+Themayanktomar@users.noreply.github.com>
2023-05-16 16:22:59 +0530

    final program ...

--- src/main/java/lambdaexamples/MaxAgeEmployee.java
+++ src/main/java/lambdaexamples/MaxAgeEmployee.java
@@ -1,24 +1,24 @@
-//package lambdaexamples;
-//
-//import assignmentques.Employee;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class MaxAgeEmployee {
-//    public static void main(String[] args) {
-//
-//        List<Employee> employees = Arrays.asList(
-//                new Employee("John", 25),
-//                new Employee("Jane", 30),
-//                new Employee("Bob", 28)
-//        );
-//
-//        int maxAge = employees.stream()
-//                .mapToInt(Employee -> Employee.getAge())
-//                .max()
-//                .orElse(0);
-//        System.out.println(maxAge);
-//
-//    }
-//}
+package lambdaexamples;
+
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MaxAgeEmployee {
+    public static void main(String[] args) {
+
+        List<Employee> employees = Arrays.asList(
+                new Employee("John", 25),
+                new Employee("Jane", 30),
+                new Employee("Bob", 28)
+        );
+
+        int maxAge = employees.stream()
+                .mapToInt(Employee -> Employee.getAge())
+                .max()
+                .orElse(0);
+        System.out.println(maxAge);
+
+    }
+}
