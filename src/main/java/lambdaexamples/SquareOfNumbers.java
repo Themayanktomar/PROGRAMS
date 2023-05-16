8312503f026aa1d1f391f1c57eb6f62531c5fd73
Themayanktomar <90128562+Themayanktomar@users.noreply.github.com>
2023-05-16 16:11:58 +0530

    elements of the array changed

--- src/main/java/lambdaexamples/SquareOfNumbers.java
+++ src/main/java/lambdaexamples/SquareOfNumbers.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class SquareOfNumbers {
     public static void main(String[] args) {
 
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numbers = Arrays.asList(4,5,8,7);
         numbers.stream()
                 .map(n -> n * n)
                 .forEach(System.out::println);
